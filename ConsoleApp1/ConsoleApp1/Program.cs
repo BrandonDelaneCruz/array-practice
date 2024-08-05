ac90f0018d01319623de2d6c910a802a123b8826
BrandonCruz <brandondelanecruz@gmail.com>
2024-08-05 02:42:31 -0500

    feat: wip

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//using System.Reflection.Metadata.Ecma335;
+
+//Console.WriteLine("Hello, World!");
+//string s = "";
+
+//int value = 'a';
+//char[] listOfChar = new char[13];
+
+//for (int i = 0; i < listOfChar.Length; i++)
+//{
+//    listOfChar[i] = (char) value++;
+//}
+
+//int numberOfErrors = 0;
+
+//char[] stringArray = s.ToCharArray();
+
+//for (int i = 0; i < stringArray.Length; i++)
+//{
+//    if (stringArray[i] != listOfChar[i])
+//    {
+//        numberOfErrors++;
+//    }
+//}
+
+//Return numberOfErrors;
+
---
+++ ConsoleApp1/ConsoleApp1/codewar.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Printer
+    {
+        public static string PrinterError(String s)
+        {
+            int value = 'a';
+            char[] listOfChar = new char[13];
+
+            for (int i = 0; i < listOfChar.Length; i++)
+            {
+                listOfChar[i] = (char)value++;
+            }
+
+            int numberOfErrors = 0;
+
+            char[] stringArray = s.ToCharArray();
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                for (int j = 0; i < listOfChar.Length; i++)
+                {
+                    if (stringArray[i] != listOfChar[j])
+                    {
+                        numberOfErrors++;
+                    }
+                }
+            }
+
+            return $"{numberOfErrors}/{s.Length}";
+        }
+
+        // s = "bbcxa"
+        public static string PrintError2(String s)
+        {
+            #region setup
+            int value = 'a';
+            char[] listOfChar = new char[13];
+
+            for (int i = 0; i < listOfChar.Length; i++)
+            {
+                listOfChar[i] = (char)value++;
+            }
+
+            int numberOfErrors = 0;
+
+            char[] stringArray = s.ToCharArray();
+            #endregion
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                bool correctChar = false;
+
+                for (int j = 0; j < listOfChar.Length; j++)
+                {
+                    var stringArrayi = stringArray[i];
+                    var listOfCharj = listOfChar[j];
+
+                    if (stringArrayi == listOfCharj)
+                    {
+                        correctChar = true;
+                        break;
+                    }
+
+                }
+
+                if (!correctChar)
+                {
+                    numberOfErrors++;
+                }
+            }
+
+            return $"{numberOfErrors}/{s.Length}";
+        }
+    }
+} 
